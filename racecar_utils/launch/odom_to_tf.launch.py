cdfb7369fb8903a4ffada607f47fcfd06c5c636d
Lawrence Shieh <lawrence92@berkeley.edu>
2023-11-20 23:55:58 -0800

    modify launch file to play ros bag, open rviz, launch perception stack, and run validation node

--- racecar_utils/launch/odom_to_tf.launch.py
+++ racecar_utils/launch/odom_to_tf.launch.py
@@ -8,17 +8,19 @@ from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
 import yaml
-from launch.substitutions import EnvironmentVariable
+from launch.substitutions import EnvironmentVariable, TextSubstitution
 import pathlib
 import launch.actions
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration
 
 def generate_launch_description():
-
+    print("Share Directory:")
+    print()
     share_dir = get_package_share_directory('racecar_utils')
     launch_dir = os.path.join(share_dir, 'launch')
     urdf = os.path.join(launch_dir, 'av21.urdf')
     rviz_path = os.path.join(launch_dir, 'lidar_tf.rviz')
+    param_path = os.path.join(get_package_share_directory('perception_evaluation'), "params.yaml")
 
     with open(urdf, 'r') as infp:
         robot_desc = infp.read()
@@ -31,11 +33,44 @@ def generate_launch_description():
     opp_topic_arg = DeclareLaunchArgument("opp_topic", default_value="", description="topic name for opp odometry")
     
     use_sim_time=DeclareLaunchArgument("use_sim_time", default_value="false")
+    param_arg = DeclareLaunchArgument('param_path', default_value="default_value", description='Param Path')
+    update_param_arg = SetLaunchConfiguration('param_path', TextSubstitution(text=param_path))
+
     arg_list.append(use_sim_time)
     arg_list.append(ego_topic_arg)
     arg_list.append(opp_topic_arg)
+    arg_list.append(param_arg)
+    arg_list.append(update_param_arg)
 
     node_list= []
+
+    # launch rosbag node
+    # param_path = "/home/group1tracker/race_common/src/perception/IAC_Perception/src/perception_evaluation/perception_evaluation/params.yaml"
+
+
+
+    with open(param_path, 'r') as file:
+        params = yaml.safe_load(file)
+        print("BAGPATH")
+        print(f"{params['rosbag_info']['BAG_FILE_PATH']}")
+        try:
+            bag_file_path = params["rosbag_info"]["BAG_FILE_PATH"]
+        except:
+            bag_file_path = "/home/group1tracker/racecar_ws/data/RACECAR-ROS2/S6/T-MULTI-FAST-POLI"
+        with open(bag_file_path + "/metadata.yaml", 'r') as file2:
+            meta_yaml = yaml.safe_load(file2)
+            params["rosbag_info"]["START_TIME"] = meta_yaml["rosbag2_bagfile_information"]["starting_time"]["nanoseconds_since_epoch"]
+    with open(param_path, 'w') as file:
+        yaml.dump(params, file)
+    
+    node_list.append(
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'bag', 'play', bag_file_path, "--clock", "100.0"],
+            name = "player"
+        )
+    )
+    
+
     node_list.append(Node(
         package='racecar_utils',
         executable='odom_to_tf_node',
@@ -56,7 +91,7 @@ def generate_launch_description():
         'robot_description': robot_desc,
     }]
     ))
-
+    # Launch rviz2 Node 
     node_list.append(Node(
             package    = 'rviz2',
             namespace  = 'lead',
@@ -64,5 +99,24 @@ def generate_launch_description():
             name       = 'rviz2',
             arguments  = ['-d' + str(rviz_path)] 
     )),
+
+    # Launch Perception Node 
+    node_list.append(
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'launch', 'autonomy_launch', "perception.launch.py"],
+            name = "perception_launcher"
+        )
+    )
+
+    # Launch Validation node:
+    node_list.append(Node(
+        package    = 'perception_evaluation',
+        # namespace  = 'lead',
+        executable = 'validation_node',
+        name       = 'validation',
+        # arguments  = ['-d' + str(param_path)] 
+        parameters = [{'param_yaml_path': LaunchConfiguration('param_path')}]
+    ))
+
 
     return LaunchDescription(arg_list+node_list)
